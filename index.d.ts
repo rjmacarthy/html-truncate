ab8e2b994fdcae70b586519037e9d6e16b7025f0
rjmacarthy <richardmacarthy@hotmail.com>
2023-04-21 10:58:16 +0100

    add declaration file

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare module 'html-truncate' {
+  /**
+   * Truncates the given HTML string to the specified length.
+   * @param html The HTML string to truncate.
+   * @param length The maximum length of the truncated HTML string.
+   * @returns The truncated HTML string.
+   */
+  function truncate(html: string, length: number): string;
+
+  export = truncate;
+}
