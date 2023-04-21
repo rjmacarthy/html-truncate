a611005d4bed0269d87b69d8639384aeef9a97ac
rjmacarthy <richardmacarthy@hotmail.com>
2023-04-21 14:22:03 +0100

    fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const truncate = (html, length) => {
     if (length <= 0) {
       return { length, text: "", ellipsisAdded: false };
     }
-
+  
     if (node.type === "text") {
       const content = node.data;
       const usedLength = Math.min(length, content.length);
@@ -21,12 +21,12 @@ const truncate = (html, length) => {
       const ellipsis = length > usedLength && isLast ? "..." : "";
       return { length: length - usedLength, text: newText + ellipsis, ellipsisAdded: !!ellipsis };
     }
-
+  
     if (node.type === "tag") {
       const children = node.children || [];
       let result = "";
       let ellipsisAdded = false;
-
+  
       for (const [index, child] of children.entries()) {
         const isLastChild = isLast && index === children.length - 1;
         const { length: newLength, text, ellipsisAdded: childEllipsisAdded } = traverse(child, length, isLastChild);
@@ -35,25 +35,30 @@ const truncate = (html, length) => {
         ellipsisAdded = childEllipsisAdded;
         if (length <= 0) break;
       }
-
+  
+      if (!ellipsisAdded && isLast) {
+        const lastTextNode = _.findLast(children, { type: "text" });
+        if (lastTextNode) {
+          const lastTextNodeIndex = result.lastIndexOf(lastTextNode.data);
+          result = result.slice(0, lastTextNodeIndex) + lastTextNode.data + "..." + result.slice(lastTextNodeIndex + lastTextNode.data.length);
+        } else {
+          result += "...";
+        }
+        ellipsisAdded = true;
+      }
+  
       const attrs = _.join(
         _.map(_.entries(node.attribs), ([key, value]) => `${key}="${value}"`),
         " "
       );
-
+  
       const attrsStr = attrs.length ? ` ${attrs}` : "";
-
+  
       const openTag = `<${node.name}${attrsStr}>`;
       const closeTag = `</${node.name}>`;
-
-      if (!ellipsisAdded && isLast) {
-        result += "...";
-        ellipsisAdded = true;
-      }
-
       return { length, text: openTag + result + closeTag, ellipsisAdded };
     }
-
+  
     return { length, text: "", ellipsisAdded: false };
   };
 
