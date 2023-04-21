b5008df651eb6928e5513d7050df4a3a6b81b964
rjmacarthy <richardmacarthy@hotmail.com>
2023-04-21 14:17:25 +0100

    revert

--- index.js
+++ index.js
@@ -1,101 +1,64 @@
-import _ from "lodash";
-import * as htmlparser2 from "htmlparser2";
-
-const truncate = (html, length) => {
-  const handler = new htmlparser2.DomHandler();
-  const parser = new htmlparser2.Parser(handler, { xmlMode: false });
-  parser.write(html);
-  parser.end();
-
-  const document = handler.dom;
-
-  const traverse = (node, length, isLast) => {
-    if (length <= 0) {
-      return { length, text: "", ellipsisAdded: false };
-    }
-
-    if (node.type === "text") {
-      const content = node.data;
-      const usedLength = _.min([length, content.length]);
-      const newText = _.take(content, usedLength).join("");
-      const ellipsis = length > usedLength && isLast ? "..." : "";
-      return {
-        length: length - usedLength,
-        text: newText + ellipsis,
-        ellipsisAdded: !!ellipsis,
-      };
-    }
-
-    if (node.type === "tag") {
-      const children = node.children || [];
-      let result = "";
-      let ellipsisAdded = false;
-
-      _.forEach(children, (child, index) => {
-        const isLastChild = isLast && index === children.length - 1;
-        const {
-          length: newLength,
-          text,
-          ellipsisAdded: childEllipsisAdded,
-        } = traverse(child, length, isLastChild);
-        length = newLength;
-        result += text;
-        ellipsisAdded = childEllipsisAdded;
-        return length > 0; // breaks the loop when length <= 0
-      });
-
-      if (!ellipsisAdded && isLast) {
-        const lastTextNode = _.findLast(children, { type: "text" });
-        if (lastTextNode) {
-          const lastTextNodeIndex = _.lastIndexOf(result, lastTextNode.data);
-          result =
-            result.slice(0, lastTextNodeIndex) +
-            lastTextNode.data +
-            "..." +
-            result.slice(lastTextNodeIndex + lastTextNode.data.length);
-        } else {
-          result += "...";
-        }
-        ellipsisAdded = true;
-      }
-
-      const attrs = _.join(
-        _.map(_.entries(node.attribs), ([key, value]) => `${key}="${value}"`),
-        " "
-      );
-
-      const attrsStr = attrs.length ? ` ${attrs}` : "";
-
-      const openTag = `<${node.name}${attrsStr}>`;
-      const closeTag = `</${node.name}>`;
-      return { length, text: openTag + result + closeTag, ellipsisAdded };
-    }
-
+const traverse = (node, length, isLast) => {
+  if (length <= 0) {
     return { length, text: "", ellipsisAdded: false };
-  };
+  }
 
-  const { text: truncatedHtml } = _.reduce(
-    document,
-    ({ text, length, ellipsisAdded }, node, index) => {
-      if (length <= 0) return { text, length, ellipsisAdded };
+  if (node.type === "text") {
+    const content = node.data;
+    const usedLength = Math.min(length, content.length);
+    const newText = content.slice(0, usedLength);
+    const ellipsis = length > usedLength && isLast ? "..." : "";
+    return {
+      length: length - usedLength,
+      text: newText + ellipsis,
+      ellipsisAdded: !!ellipsis,
+    };
+  }
 
-      const isLastNode = index === document.length - 1;
+  if (node.type === "tag") {
+    const children = node.children || [];
+    let result = "";
+    let ellipsisAdded = false;
+
+    for (const [index, child] of children.entries()) {
+      const isLastChild = isLast && index === children.length - 1;
       const {
         length: newLength,
-        text: newText,
-        ellipsisAdded: nodeEllipsisAdded,
-      } = traverse(node, length, isLastNode);
+        text,
+        ellipsisAdded: childEllipsisAdded,
+      } = traverse(child, length, isLastChild);
+      length = newLength;
+      result += text;
+      ellipsisAdded = childEllipsisAdded;
+      if (length <= 0) break;
+    }
 
-      return {
-        text: text + newText,
-        length: newLength,
-        ellipsisAdded: nodeEllipsisAdded,
-      };
-    },
-    { text: "", length, ellipsisAdded: false }
-  );
+    if (!ellipsisAdded && isLast) {
+      const lastTextNode = _.findLast(children, { type: "text" });
+      if (lastTextNode) {
+        const lastTextNodeIndex = result.lastIndexOf(lastTextNode.data);
+        result =
+          result.slice(0, lastTextNodeIndex) +
+          lastTextNode.data +
+          "..." +
+          result.slice(lastTextNodeIndex + lastTextNode.data.length);
+      } else {
+        result += "...";
+      }
+      ellipsisAdded = true;
+    }
 
-  return truncatedHtml;
+    const attrs = _.join(
+      _.map(_.entries(node.attribs), ([key, value]) => `${key}="${value}"`),
+      " "
+    );
+
+    const attrsStr = attrs.length ? ` ${attrs}` : "";
+
+    const openTag = `<${node.name}${attrsStr}>`;
+    const closeTag = `</${node.name}>`;
+    return { length, text: openTag + result + closeTag, ellipsisAdded };
+  }
+
+  return { length, text: "", ellipsisAdded: false };
 };
-
-export default truncate;
